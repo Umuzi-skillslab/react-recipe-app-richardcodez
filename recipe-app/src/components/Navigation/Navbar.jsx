96ddbc303d262e2d482b2bcb8515614023c90250
richardcodez <richardcodez23@gmail.com>
2026-08-28 11:35:42 +0200

    navbar implemented

--- recipe-app/src/components/Navigation/Navbar.jsx
+++ recipe-app/src/components/Navigation/Navbar.jsx
@@ -1,3 +1,30 @@
-export default function Navbar() {
-    return <></>
+import { NavLink, useLocation } from "react-router-dom";
+
+const Navbar = () => {
+    const location = useLocation();
+
+    return (
+        <>
+            <nav className="navbar">
+                <NavLink 
+                    to="/" 
+                    className={location.pathname === '/' ? StyleSheet.active : ""}
+                >Home</NavLink>
+                <NavLink
+                    to="/recipes"
+                    className={location.pathname.includes('/recipes') ? StyleSheet.active : ""}
+                >Recipes</NavLink>
+                <NavLink
+                    to="/meal-planner"
+                    className={location.pathname === '/meal-planner' ? StyleSheet.active : ""}
+                >Meal Planner</NavLink>
+                <NavLink
+                    to="/favorites"
+                    className={location.pathname === '/favorites' ? StyleSheet.active : ""}
+                >Favorites</NavLink>
+            </nav>
+        </>
+    );
 }
+
+export default Navbar;
